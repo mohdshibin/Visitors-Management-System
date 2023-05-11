00b80cc06b19d2980eeb8867ac588413e1a243b9
mohammed.shibin <mohammed.shibin@OPN-MC-030.local>
2023-05-11 10:38:01 +0530

    created login and register template

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.base')
+
+@section('title', 'Login')
+
+@section('content')
+
+  <div class= "container">
+    <form class="mx-auto mt-2" style="width: 600px; height: 100%">
+        <div class="mb-3">
+            <label class="form-label">Email address</label>
+            <input type="email" class="form-control" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Password</label>
+            <input type="password" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+  </div>
+
+@stop
